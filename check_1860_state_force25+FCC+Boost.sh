2e9ce0a32088d723fd029dc66897b5fefe628a4d
Evgeniy <36340702+jkson5@users.noreply.github.com>
2018-12-02 14:18:44 +0300

    Update check_1860_state_force25+FCC+Boost.sh

--- check_1860_state_force25+FCC+Boost.sh
+++ check_1860_state_force25+FCC+Boost.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
+/system/bin/check_1860_state.sh&
+sleep 10
 dji_mb_ctrl -S test -R local -g 9 -s 9 -c 27 00024800FFFF0200000000
 dji_mb_ctrl -S test -R local -g 9 -s 9 -c 3c
 dji_mb_ctrl -S test -R local -g 9 -s 9 -c 27 00014600FFFF92040000
-/system/bin/check_1860_state.sh&
