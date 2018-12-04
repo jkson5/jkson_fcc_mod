3ac2f0e70fe1229ef957b59f64c5da52a4be32f8
Evgeniy <36340702+jkson5@users.noreply.github.com>
2018-12-04 19:34:54 +0300

    Delete check_1860_state_force23+FCC.sh

--- check_1860_state_force23+FCC.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-/system/bin/check_1860_state.sh&
-sleep 10
-dji_mb_ctrl -S test -R local -g 9 -s 9 -c 27 00024800FFFF0200000000
-dji_mb_ctrl -S test -R local -g 9 -s 9 -c 27 00014600FFFFA2030000
